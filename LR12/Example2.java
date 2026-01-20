332d4d7618dba1fb608ad9c503634116375043f3
Pavel <119424848+PavelShlyapnikov@users.noreply.github.com>
2026-01-20 17:23:01 +0500

    Add files via upload

---
+++ LR12/Example2.java
@@ -0,0 +1,23 @@
+package LR12;
+
+/*
+2. Напишите программу, которая запускает поток и выводит на экран числа от 1 до 10 с задержкой в 1 секунду
+между выводами.
+ */
+
+public class Example2 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("Error: " + e);
+                }
+            }
+        });
+        thread.start();
+        thread.join();
+    }
+}
